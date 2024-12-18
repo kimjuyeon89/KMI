8c61b18f9e72094dabbfc534437a872a8be42628
kimjuyeon89 <jy8968@gmail.com>
2024-12-18 11:24:53 +0900

    push

--- pre-build/gulpfile.js
+++ pre-build/gulpfile.js
@@ -1,89 +1,136 @@
-// 모듈 가져오기
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const fileinclude = require('gulp-file-include');
-const scss = require('gulp-sass')(require('sass')); // SCSS 모듈
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const cached = require('gulp-cached');
-const autoPrefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+/*var gulp = require('gulp');
+var concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    fileinclude = require('gulp-file-include'),
+    scss = require('gulp-sass'); // sass 호출
+    sourcemaps = require('gulp-sourcemaps'), //sourcemaps 호출
+    browserSync = require('browser-sync').create();*/
 
-// 경로 설정
-const src = {
-    html: ['html/**/*.html'],
-    js: ['js/*.js', 'js/**/*.js'],
-    css: ['scss/*.scss', 'scss/**/*.scss'],
-    imgs: ['images/**'],
-};
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import fileinclude from 'gulp-file-include';
+import scss from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import browserSync from 'browser-sync';
+import cached from 'gulp-cached';
+import autoPrefixer from 'gulp-autoprefixer';
 
-const paths = {
-    html: 'build/html/',
-    js: 'build/js/',
-    css: 'build/css/',
-    imgs: 'build/images/',
-};
-
-// SCSS 옵션
-const scssOptions = {
-    outputStyle: 'compressed', // 압축된 CSS
-    indentType: 'space',
-    indentWidth: 2,
-    precision: 6,
-    sourceComments: false,
-};
-
-// HTML 컴파일
-function htmlCompile() {
-    return gulp
-        .src(src.html)
-        .pipe(fileinclude({ prefix: '@@', basepath: '@file' }))
-        .pipe(gulp.dest(paths.html));
+var src = {
+    html : ['html/**/*.html','html/*.html','html/**/*.html'],
+    include : ['html/include/*.html'],
+    js : ['js/*.js','js/**/*.js'],
+    css : ['scss/*.scss','scss/**/*.scss'],
+    imgs : ['images/**','images/**/**','images/**/**/*']
+}
+var paths = {
+    html: '../build/html/',
+    include : '../build/html/include/',
+    js : '../build/js/',
+    css : '../build/css/',
+    imgs : '../build/images/'
 }
 
-// SCSS 컴파일
+var scssOptions = {
+    /** * outputStyle (Type : String , Default : nested) * CSS의 컴파일 결과 코드스타일 지정 * Values : nested, expanded, compact, compressed */
+    outputStyle : "expanded",
+
+    /** * indentType (>= v3.0.0 , Type : String , Default : space) * 컴파일 된 CSS의 "들여쓰기" 의 타입 * Values : space , tab */
+    indentType : "tab",
+
+    /** * indentWidth (>= v3.0.0, Type : Integer , Default : 2) * 컴파일 된 CSS의 "들여쓰기" 의 갯수 */
+    indentWidth : 1,
+
+    /** * outputStyle 이 nested, expanded 인 경우에 사용 /** * precision (Type : Integer , Default : 5) * 컴파일 된 CSS 의 소수점 자리수. */
+    precision: 6,
+
+    /** * sourceComments (Type : Boolean , Default : false) * 컴파일 된 CSS 에 원본소스의 위치와 줄수 주석표시. */
+    sourceComments: true
+};
+
+gulp.task('build', gulp.series(htmlComplie, includeComplie, scssCompile, concatJs, imgs));
+
+function htmlComplie() {
+    return gulp.src(src.html)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file',
+            indent: true
+        }))
+        .pipe(cached('html'))
+        .pipe(gulp.dest(paths.html))
+        .pipe(browserSync.reload({stream:true}))
+};
+
+function includeComplie() {
+    return gulp.src(src.include)
+        .pipe(gulp.dest(paths.include))
+        .pipe(htmlComplie())
+        .pipe(browserSync.reload({stream:true}));
+}
+
+
 function scssCompile() {
-    return gulp
-        .src(src.css)
+    return gulp.src(src.css)
+        .pipe(cached('css'))
+
+        // 소스맵 초기화(소스맵을 생성)
         .pipe(sourcemaps.init())
+
+        // SCSS 함수에 옵션갑을 설정, SCSS 작성시 watch 가 멈추지 않도록 logError 를 설정
         .pipe(scss(scssOptions).on('error', scss.logError))
         .pipe(autoPrefixer())
-        .pipe(cleanCSS())
+
+        // 위에서 생성한 소스맵을 사용한다.
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.css));
+
+        // 목적지(destination)을 설정
+        .pipe(gulp.dest(paths.css))
+
+        //browserSync 로 브라우저에 반영;
+        .pipe(browserSync.reload({stream:true}));
+
+};
+
+
+function concatJs() {
+    return gulp.src(src.js)
+        // .pipe(concat('ui.js'))
+        // .pipe(gulp.dest(paths.js))
+        // .pipe(uglify())
+        // .pipe(rename('ui.min.js'))
+        //.pipe(babel())
+        .pipe(cached('js'))
+        .pipe(gulp.dest(paths.js))
+        .pipe(browserSync.reload({stream:true}));
+
 }
 
-// JS 처리
-function jsCompile() {
-    return gulp
-        .src(src.js)
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(paths.js));
+function imgs() {
+    return gulp.src(src.imgs)
+        .pipe(cached('imgs'))
+        .pipe(gulp.dest(paths.imgs))
+        .pipe(browserSync.reload({stream:true}));
 }
 
-// 이미지 복사
-function imgCopy() {
-    return gulp.src(src.imgs).pipe(gulp.dest(paths.imgs));
+function watchFiles(){
+    gulp.watch(src.html).on('change',htmlComplie);
+    gulp.watch(src.include).on('change',includeComplie);
+    gulp.watch(src.css, scssCompile);
+    gulp.watch(src.js, concatJs);
+    gulp.watch(src.imgs, imgs);
+
 }
 
-// 빌드 작업 (Netlify 배포용)
-const build = gulp.series(htmlCompile, scssCompile, jsCompile, imgCopy);
-
-// 기본 작업 (로컬 개발용)
-function watchFiles() {
+function brwSync(){
     browserSync.init({
-        server: { baseDir: 'build/', index: 'html/index.html' },
+        server:{
+            baseDir:'../build/',
+            index:'html/00_coding_list.html'
+        }
     });
-
-    gulp.watch(src.html, htmlCompile).on('change', browserSync.reload);
-    gulp.watch(src.css, scssCompile).on('change', browserSync.reload);
-    gulp.watch(src.js, jsCompile).on('change', browserSync.reload);
-    gulp.watch(src.imgs, imgCopy).on('change', browserSync.reload);
 }
 
-exports.build = build; // Netlify에서 실행할 빌드 명령어
-exports.default = gulp.series(build, watchFiles); // 로컬 개발용
+gulp.task('default', gulp.parallel(gulp.series(htmlComplie,includeComplie, scssCompile, concatJs, imgs),brwSync, watchFiles));
